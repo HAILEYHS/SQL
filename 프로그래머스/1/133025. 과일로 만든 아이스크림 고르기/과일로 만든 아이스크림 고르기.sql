9b52feba791c023706bd6552ef313afe510b1d9d
JangHyosun <137710845+HAILEYHS@users.noreply.github.com>
2024-12-22 23:29:47 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,6 @@
+-- MySQL
+SELECT a.flavor
+from FIRST_HALF a
+join ICECREAM_INFO b on a.flavor = b.flavor
+where a.total_order > 3000 and b.ingredient_type = 'fruit_based'
+order by total_order desc;
